9259217abb7c7ba73819031c5928b4f0ecf19e4a
Sebastian Prodan <sebiprodan@gmail.com>
2018-01-20 20:11:05 +0200

    CoinFlipping (new program)

---
+++ src/CoinFlipping.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CoinFlipping {
+
+    public static void main(String[] args) {
+
+        String[] posibilitiy= {"Stema", "Ban"};
+
+        Random random = new Random();
+
+        String coinFlipping = posibilitiy[random.nextInt(posibilitiy.length)];
+
+        System.out.println(coinFlipping);
+    }
+}
